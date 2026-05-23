4e1a884002ab0264e0622e2e89694c3a34d4f075
asathk <asathk2006@gmail.com>
2026-05-23 12:14:44 +0530

    first

--- c2.c
+++ c2.c
@@ -1,82 +1,97 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 struct node
 {
-  int data;
-  char data1;
-  struct node*next;
-  struct node*prev;
-  };
-  struct node*head=NULL;
-  struct node*tail=NULL;
- void insertid(int id)
- {
-    struct node*nn=(struct node*)malloc(sizeof(int));
-    nn->data=id;
-    nn->next=NULL;
-    if(head==NULL)
+    int data;
+    char data1[20];
+
+    struct node *next;
+    struct node *prev;
+};
+
+struct node *head = NULL;
+struct node *tail = NULL;
+
+void insertid(int id)
+{
+    struct node *nn = (struct node*)malloc(sizeof(struct node));
+
+    nn->data = id;
+    strcpy(nn->data1, "");
+
+    nn->next = NULL;
+    nn->prev = NULL;
+
+    if(head == NULL)
     {
-       head=tail=nn;
-       head->prev=NULL;
-     }
-     else
-     {
-       nn->next=head;
-       head->prev=nn;
-       head=nn;
-      }
+        head = tail = nn;
     }
-   void insertname(char name)
-   {
-      struct node*nn=(struct node*)malloc(sizeof(char));
-      strlen(nn->data1)=strlen(name);
-      nn->next=NULL;
-      if(head==NULL)
-      {
-         head=tail=nn;
-         }
-         else
-         {
-          nn->next=head;
-          head->prev=NULL;
-          head=nn;
-          }
-         }
-   
-  void display()
-  {
-      struct node*temp=head;
-      if(head==NULL)
-      {
-        printf("list is empty");
+    else
+    {
+        nn->next = head;
+        head->prev = nn;
+        head = nn;
+    }
+}
+
+void insertname(char name[])
+{
+    struct node *nn = (struct node*)malloc(sizeof(struct node));
+
+    nn->data = 0;
+    strcpy(nn->data1, name);
+
+    nn->next = NULL;
+    nn->prev = NULL;
+
+    if(head == NULL)
+    {
+        head = tail = nn;
+    }
+    else
+    {
+        nn->next = head;
+        head->prev = nn;
+        head = nn;
+    }
+}
+
+void display()
+{
+    struct node *temp = head;
+
+    if(head == NULL)
+    {
+        printf("List is empty\n");
         return;
-       }
-       else
-       {
-           while(temp!=NULL)
-           {
-                 printf("%d",temp->data);
-                  temp=temp->next;
-            }
-        } 
-        printf("\n");
-       }
-       
-    
+    }
+
+    while(temp != NULL)
+    {
+        printf("ID = %d , Name = %s\n",
+               temp->data,
+               temp->data1);
+
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
+
 int main()
 {
- 
-  char name[20];
-  int id;
-  char st[200];
-  scanf("%s",name);
-  scanf("%d",&id);
-  scanf("%s",st);
-  insertid(id);
-  display();
-  insertname(name);
-  display();
-  return 0;
-  
-  }
+    char name[20];
+    int id;
+
+    scanf("%s", name);
+    scanf("%d", &id);
+
+    insertid(id);
+    insertname(name);
+
+    display();
+
+    return 0;
+}
